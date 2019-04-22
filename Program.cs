dfe8073b822db90acdfdb53425de260d09defadc
Jared Farkas and megan schulte <jared@>
2019-04-22 10:42:17 -0700

    refactor code

--- Program.cs
+++ Program.cs
@@ -20,43 +20,44 @@ public class Program //basic boilerplate code
         int guess = int.Parse(Console.ReadLine());
         if(guess == num)
         {
-            Console.WriteLine("You guessed it");
-            guessed = true;
-        }else{
+
+          Console.WriteLine("You guessed it");
+          guessed = true;
+        } else
+          {
           if(guess < num){
             Console.WriteLine("Higher");
-          }else{
+          } else
+          {
             Console.WriteLine("Lower");
+            }
           }
+        }
+      } else
+      {
+        Console.WriteLine("Think of a number between 1-100.");
+        bool guessed = false;
+        Random random = new Random();
+        int num = random.Next(1,100);
+        int min = 1;
+        int max = 100;
 
+        while(guessed == false){
+          Console.WriteLine("Okay, is your number " + num + "? Enter higher/lower/correct");
+          string response = Console.ReadLine();
+
+          if (response == "higher"){
+            min = num+1;
+            num = random.Next(min, max);
+          } else if (response == "lower"){
+            max = num-1;
+            num = random.Next(min, max);
+          } else {
+            Console.WriteLine("Got it! Eat it jerk!");
+            guessed = true;
+          }
         }
       }
-    } else
-     {
-       Console.WriteLine("Think of a number between 1-100.");
-        bool guessed = false;
-       Random random = new Random();
-       int num = random.Next(1,100);
-       int min = 1;
-       int max = 100;
-
-       while(guessed == false){
-         Console.WriteLine("Okay, is your number " + num + "? Enter higher/lower/correct");
-         string response = Console.ReadLine();
-
-         if (response == "higher"){
-           min = num+1;
-           num = random.Next(min, max);
-           }else if (response == "lower"){
-             max = num-1;
-             num = random.Next(min, max);
-           } else {
-             Console.WriteLine("Got it! Eat it jerk!");
-             guessed = true;
-           }
-       }
-
-    }
     }
   }
 }
